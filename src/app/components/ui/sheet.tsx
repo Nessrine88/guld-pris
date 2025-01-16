3f4fd4594c4492952d477659ff94187485b1308b
Nessrine88 <macherki.nessrine@yahoo.com>
2025-01-16 14:52:40 +0100

    make the navbar responsive

--- src/app/components/ui/sheet.tsx
+++ src/app/components/ui/sheet.tsx
@@ -14,11 +14,16 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
-  className,
-  ...props
-}: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
-)
+    className,
+    ...props
+  }: SheetPrimitive.DialogPortalProps & { className?: string }) => (
+    <SheetPrimitive.Portal {...props}>
+      <div className={cn(className)}>
+        {props.children}
+      </div>
+    </SheetPrimitive.Portal>
+  )
+  
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
 const SheetOverlay = React.forwardRef<
